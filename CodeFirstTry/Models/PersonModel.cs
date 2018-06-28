1c30d959c8cf754ca2c7635546f58795f28eb20d
Daniel <segibambo@gmail.com>
2018-06-28 13:02:53 +0100

    Data Annotations added to model

--- CodeFirstTry/Models/PersonModel.cs
+++ CodeFirstTry/Models/PersonModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +10,16 @@ namespace CodeFirstTry.Models
     {
         public int PersonID { get; set; }
 
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
 
+        [Display(Name = "Date of Birth")]
+        [Required(ErrorMessage = "Date of birth name is required")]
         public DateTime DateofBirth { get; set; }
 
         public virtual ICollection<Family> Families { get; set; }
